1ca98156bf67da145d033f4d7dde6d9a8b0058d8
TahirMansuri <tprocks786@gmail.com>
2022-02-07 20:40:07 +0530

    MODIFIED: Add Hotels to Hotel Reservation System

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -4,5 +4,8 @@ public class HotelReservationSystem {
     public static void main(String[] args) {
         //Welcome message for User
         System.out.println("Welcome to Hotel Reservation System Workshop.");
+
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addHotel();
     }
 }
